c4523fb60cb2fad6f6cd40942f1493989cfa3b1f
Rex <yongxin1@ualberta.ca>
2025-01-18 22:59:08 -0700

    docker-compose setting update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  // change port to 3001
+  server: {
+    port: 3001, 
+  },
+
 })
