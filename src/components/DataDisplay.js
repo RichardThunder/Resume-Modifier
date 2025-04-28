312ab2e5c242e339b3bcfecd616d01cd89c1e6fc
Richard <yuetingpei888@gmail.com>
2025-04-28 14:18:40 +0800

    页面语言统一使用英文

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -22,7 +22,7 @@ const DataDisplay = () => {
           onClick={toggleExpand}
           className="px-3 py-1 bg-blue-100 text-blue-700 rounded hover:bg-blue-200 text-sm"
         >
-          {isExpanded ? '收起' : '展开'}
+          {isExpanded ? 'Collapse' : 'Expand'}
         </button>
       </div>
       
